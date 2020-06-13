166fdf83f11ca26fcab804940b953ca553866a0c
Igor Correia Freitas <igorcfreittas@pop-os.localdomain>
2020-06-13 11:43:22 -0300

    Adicionando o domínio do projeto para o usuário padrão do container php-fpm/www-data

--- launch_application.sh
+++ launch_application.sh
@@ -9,6 +9,11 @@ docker-compose up --build -d
 cp applications/easy_stock/.env.example applications/easy_stock/.env
 docker exec -it php composer install
 docker exec -it php php artisan key:generate
+docker exec -it php php artisan migrate
 docker exec -it php php artisan db:seed
+
+# Add permission to default php-fpm user
+docker exec -it php chown -R www-data:www-data /var/www/html
+docker exec -it php chmod -R 755 /var/www/html/storage
 
 echo "\nBuild OK\n"
